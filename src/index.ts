0a4525a3b05bfc915cd71e2dd0158095be9e473d
Sujan Shrestha <shrsujan2007@gmail.com>
2018-06-05 18:45:36 +0545

    Add morgan package for logging api calls

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'babel-polyfill';
 
+import * as morgan from 'morgan';
 import * as cors from 'cors';
 import * as helmet from 'helmet';
 import * as express from 'express';
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(helmet());
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
